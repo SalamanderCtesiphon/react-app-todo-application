f99040282b9c7db3879f2959ebc31e2fe3e205ba
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-29 12:24:36 -0600

    copied the code from the react documentation in order to follow along with the examples

--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
 );
+
+function tick() {
+  const element = (
+    <div className='App-header'>
+      <h1>Hello, world!</h1>
+      <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+  );
+  root.render(element);}
+
+setInterval(tick, 1000);
+
+
 
