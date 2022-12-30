ace23a384b8169dcaa40778d3c7a2779c65087d7
SalamanderCtesiphon <swbrookshire@gmail.com>
2022-12-30 12:09:35 -0600

    copied the files from the lesson on passing props in react

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import MyComponent from './MyComponent';
 
 
 
@@ -15,6 +16,7 @@ function tick() {
       <h1>Hello, world!</h1>
       <h2>It is {new Date().toLocaleTimeString()}.</h2>
       <App />
+      <MyComponent />
     </div>
   );
   root.render(element);}
