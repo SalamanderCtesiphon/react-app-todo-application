692a51f47e74f74849245a367add306dbe633c77
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-02 12:19:27 -0600

    following along with the react documentation

--- src/index.js
+++ src/index.js
@@ -10,15 +10,22 @@ const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 
-function tick() {
-  const element = (
-    <div className='App-header'>
+function Clock(props) {
+  return (
+    <div>
       <h1>Hello, world!</h1>
-      <h2>It is {new Date().toLocaleTimeString()}.</h2>
-      <App />
+      <h2>It is {props.date.toLocaleTimeString()}.</h2>
     </div>
   );
-  root.render(element);}
+}
+
+function tick() {
+  root.render(<div>
+    <Clock date={new Date()}/>
+    <App/>
+  </div>
+    );
+}
 
 setInterval(tick, 1000);
 
