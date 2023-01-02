0270031efbf2d2f8d90a8d86ab3b4930439deb73
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-02 12:31:33 -0600

    finished up tutorial

--- src/index.js
+++ src/index.js
@@ -12,11 +12,15 @@ const root = ReactDOM.createRoot(
 
 
 class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
   render() {
     return (
       <div>
         <h1>Hello, world!</h1>
-        <h2>It is {this.props.date.toLocaleTimeString()}.</h2>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
       </div>
     )
   }
@@ -24,7 +28,7 @@ class Clock extends React.Component {
 
 function tick() {
   root.render(<div>
-    <Clock date={new Date()}/>
+    <Clock />
     <App/>
   </div>
     );
