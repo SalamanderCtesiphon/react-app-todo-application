73823fc0d5d21bb7f65368bb1ee29f062b2c073e
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-14 13:30:22 -0600

    cleared up the directories and deleted unused files and code

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
 
 class App extends Component {
-  constructor() {
-    super();
-
-    this.state = {
-      count: 0,
-    };
-
-    this.countUp = this.countUp.bind(this);
-  }
-
-  countUp() {
-    this.setState({
-      count: this.state.count + 1,
-    });
-  }
-
   render() {
     return (
-      <div>
-        <button onClick={this.countUp}>Click Me!</button>
-        <p>{this.state.count}</p>
+      <div className='App'>
+        <div className='container'>
+          <Header />
+        </div>  
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,40 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 
 
-
-
-const root = ReactDOM.createRoot(
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 
-class Clock extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {date: new Date()};
-  }
-  render() {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
-      </div>
-    )
-  }
-}
-
-function tick() {
-  root.render(<div>
-    <Clock />
-    <App/>
-  </div>
-    );
-}
-
-setInterval(tick, 1000);
 
 
 
