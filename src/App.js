424950bea35e764fe4cc1c362e12640f778e94cd
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-14 14:53:09 -0600

    have a working prototype

--- src/App.js
+++ src/App.js
@@ -1,15 +1,63 @@
-import React, { Component } from 'react';
-import Header from './components/Header';
+import React, { Component } from "react";
+import Header from "./components/Header";
+import Overview from "./components/Overview";
+import uniqid from "uniqid";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      task: {
+        text: '',
+        id: uniqid()
+      },
+      tasks: [],
+    };
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      task : {
+        text: e.target.value,
+        id: this.state.task.id,
+      }
+    });
+  };
+  
+  onSubmitTask = (e) => {
+    e.preventDefault();
+    this.setState({
+      tasks: this.state.tasks.concat(this.state.task),
+      task: { 
+        text: '',
+        id: uniqid()
+      },
+    });
+  };
+  
+
   render() {
+    const { task, tasks } = this.state;
+
     return (
-      <div className='App'>
-        <div className='container'>
-          <Header />
-        </div>  
+      <div>
+        <Header />
+        <form onSubmit={this.onSubmitTask}>
+          <label htmlFor="taskInput">Enter task</label>
+          <input 
+          onChange={this.handleChange}
+          value={task.text}
+          type="text" 
+          id="taskInput"
+          /> {''}
+          <button type="submit">
+            Add Task
+          </button>
+        </form>
+        <Overview tasks={tasks} />
       </div>
-    )
+    );
   }
 }
 
