9693823eabff123e5e6f58636027385f361864a3
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-14 13:53:51 -0600

    add a style folder and added an external stylesheet

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,21 @@
 import React, { Component } from 'react'
+import '../styles/HeaderStyles.css'
 
 export class Header extends Component {
   render() {
     return (
-      <div>Header</div>
+      <h1 className="header" style={headerStyle}>Task App</h1>
     )
   }
+}
+
+const headerStyle = {
+    background:'#333',
+    color: '#fff',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '6vh'
 }
 
 export default Header
