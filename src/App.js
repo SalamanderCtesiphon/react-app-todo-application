c21f5dd97438a1bb6f5828acec4d9b978b6abc51
SalamanderCtesiphon <swbrookshire@gmail.com>
2023-01-14 19:44:38 -0600

    got the delete button working

--- src/App.js
+++ src/App.js
@@ -1,74 +1,44 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
-import Overview from "./components/Overview";
+import AddTask from "./components/AddTask";
+import Tasks from "./components/Tasks";
 import uniqid from "uniqid";
+import './App.css';
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      task: {
-        number: 0,
-        text: '',
-        id: uniqid()
-      },
-      tasks: [],
-    };
-
-    this.handleChange = this.handleChange.bind(this);
-    this.onSubmitTask = this.onSubmitTask.bind(this);
-    this.delTask = this.delTask.bind(this);
+  state = {
+    tasks: [
+      {
+        number: 1,
+        id: uniqid(),
+        title: 'Get Organized'
+      }
+    ]
   }
 
   delTask = (id) => {
-    this.setState({ tasks: [...this.state.tasks.filter(task => task.id !==id)]})
+    this.setState({ 
+      tasks: [...this.state.tasks.filter(task => task.id !==id)]})
   }
   
-
-
-  handleChange = (e) => {
-    this.setState({
-      task : {
-        number: this.state.task.number + 1,
-        text: e.target.value,
-        id: this.state.task.id,
-      }
-    });
-  };
-  
-  onSubmitTask = (e) => {
-    e.preventDefault();
-    this.setState({
-      tasks: this.state.tasks.concat(this.state.task),
-      task: { 
-        number: this.state.task.number,
-        text: '',
-        id: uniqid()
-      },
-    });
-  };
-  
+  addTask = (title, number) => {
+    const newTask = {
+      id: uniqid(),
+      title,
+      number
+    }
+    this.setState({ tasks: [...this.state.tasks, newTask]})
+  }
 
   render() {
-    const { task, tasks, id } = this.state;
-
     return (
-      <div>
-        <Header />
-        <form onSubmit={this.onSubmitTask}>
-          <label htmlFor="taskInput">Enter task</label>
-          <input 
-          onChange={this.handleChange}
-          value={task.text}
-          type="text" 
-          id="taskInput"
-          /> {''}
-          <button type="submit">
-            Add Task
-          </button>
-        </form>
-        <Overview tasks={tasks} delTask={this.delTask} />
+      <div className="App">
+        <div className="container">
+          <Header />
+          <AddTask addTask={this.addTask}/>
+          <Tasks number={this.state.number} tasks={this.state.tasks} 
+          delTask={this.delTask} />
+        </div>
       </div>
     );
   }
